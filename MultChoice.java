43c40bdd9566d9608ec67902926de132463c8dc7
Rishabhshah26 <rishabh.shah26@ycdsbk12.ca>
2025-06-03 09:51:56 -0400

    unedited changes

--- MultChoice.java
+++ MultChoice.java
@@ -6,50 +6,60 @@ public class MultChoice{
 		
 		String strDecision;
 		
+		String strDecision2;
+		
 		con.println("Would you like to: A - Play a Quiz, B - View Leaderboard, C - Add Quiz, C - Quit");
 		strDecision = con.readLine();
 		
-		if(strDecision.equalsIgnoreCase("A")){
-			TextInputFile marvel = new TextInputFile("MCU.txt"); 
+		if(strDecision.equalsIgnoreCase("A")){	
+			
+			con.println("A - MCU, B - Countries, C - Sports");
+			strDecision2 = con.readLine();
+			
+			if(strDecision2.equalsIgnoreCase("A")){
+			
+			
+				TextInputFile marvel = new TextInputFile("MCU.txt"); 
 		
-			String strMCU [][];
-			strMCU = new String [13][6];
+				String strMCU [][];
+				strMCU = new String [13][6];
 		
 
-			String strQuestion;
-			String strA;
-			String strB;
-			String strC;
-			String strD;
-			String strAns = "";
-			int intBSort; 
-			int intCount = 0;
+				String strQuestion;
+				String strA;
+				String strB;
+				String strC;
+				String strD;
+				String strAns = "";
+				int intBSort; 
+				intCount = 0;
 			
-			while(marvel.eof()== false){
-				if(intCount >= 13){
-					intCount = intCount + 1;
-					strQuestion = con.readLine();
-					strA = con.readLine();
-					strB = con.readLine();
-					strC = con.readLine();
-					strD = con.readLine();
+				while(marvel.eof()== false){
+					if(intCount >= 13){
+						intCount = intCount + 1;
+						strQuestion = con.readLine();
+						strA = con.readLine();
+						strB = con.readLine();
+						strC = con.readLine();
+						strD = con.readLine();
 			
-					intBSort = (int)(Math.random()*100+1);
+						intBSort = (int)(Math.random()*100+1);
 		
 		
-					strMCU[intCount][0] = strQuestion;
-					strMCU[intCount][1] = strA;
-					strMCU[intCount][2] = strB;
-					strMCU[intCount][3] = strC;
-					strMCU[intCount][4] = strD;
-					strMCU[intCount][5] = strAns;
-					strMCU[intCount][6] = Integer.toString(intBSort);
-
+						strMCU[intCount][0] = strQuestion;
+						strMCU[intCount][1] = strA;
+						strMCU[intCount][2] = strB;
+						strMCU[intCount][3] = strC;
+						strMCU[intCount][4] = strD;
+						strMCU[intCount][5] = strAns;
+						strMCU[intCount][6] = Integer.toString(intBSort);
+	
+					}
+			
+			
+		
 				}
-			
-			
-		
-			}
 		}
+			}
 	}
 }
